41567beaadee572a4243eb2f62a5aa5b60437a3d
S. M. A Sayed Al Banna <2021591@iub.edu.bd>
2023-04-05 08:33:18 +0600

    fixed

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-users = [{username:"akib",password:"12345", type:"client"}, {username:"banna",password:"12345", type:"manager"}, {username:"ayat",password:"12345", type:"analyst"}, {username:"fahad",password:"12345", type:"developer"}];
+users = [{username:"akib",password:"12345", type:"client"}, {username:"banna",password:"12345", type:"manager"}, {username:"fahad",password:"12345", type:"analyst"}, {username:"fahad",password:"12345", type:"developer"}];
 
 function login(){
     userName = document.getElementById("username").value;
@@ -6,7 +6,7 @@ function login(){
     for(i=0;i<users.length;i++){
         if(userName==users[i].username && pass==users[i].password){
             if(users[i].type=="manager")document.getElementById('loginForm').action='Project Manager/projectManager.html';
-            else if(users[i].type=="analyst")document.getElementById('loginForm').action='Analyst/analyst.html';
+            else if(users[i].type=="analyst")document.getElementById('loginForm').action='Employee/employee.html';
             else if(users[i].type=="client") document.getElementById('loginForm').action='client/client.html';
             else if(users[i].type=="developer") document.getElementById('loginForm').action='Developer/developer.html';
             return true;
